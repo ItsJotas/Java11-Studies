54f8ac24e3cbd93188dae4e9234bc046df3de533
Jhonata Costa <costajhonata8@gmail.com>
2024-04-28 00:16:12 -0300

    Import static methods

---
+++ src/main/java/TestImportStatic.java
@@ -0,0 +1,14 @@
+import static java.lang.Math.PI;
+import static java.lang.Math.sqrt;
+public class TestImportStatic {
+
+    public static void main(String[] args){
+        Integer radius = 2;
+
+        Double circumference = 2 * PI * radius;
+        System.out.println("circumference = " + circumference);
+
+        Double sqrt81 = sqrt(81);
+        System.out.println("sqrt81 = " + sqrt81);
+    }
+}
